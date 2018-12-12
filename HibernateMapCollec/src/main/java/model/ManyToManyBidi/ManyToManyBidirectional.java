6b9357d98d52df62fa060ac2617d95b42a627bb5
root <root@root-PC>
2018-12-12 19:21:12 -0300

    12/12/2018  17.30

--- HibernateMapCollec/src/main/java/model/ManyToManyBidi/ManyToManyBidirectional.java
+++ HibernateMapCollec/src/main/java/model/ManyToManyBidi/ManyToManyBidirectional.java
@@ -51,6 +51,7 @@ public class ManyToManyBidirectional {
 		Item someItem2 = new Item("Some Item B");
 		Item someItem3 = new Item("Some Item C");
 		
+		System.out.println("Hola");
 		
 		categoria1.getItems().add(someItem1);  // CATEGORY 1 PERSISTE 3 TIPOS DE ITEMS
 		categoria1.getItems().add(someItem2);
